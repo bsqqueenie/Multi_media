7992a6a1d0671f275328b2f5967a5e69de0c83ee
Siyang Qian <syqdark@foxmail.com>
2019-09-17 13:31:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,83 @@
 import trimesh
+import tkinter as tk
+from tkinter import filedialog
 import numpy as np
 import networkx as nx
-offPath='/Users/jack/Desktop/privateStuff/UUstuff/2019-2020/period1/MR/assignment/benchmark/db/0/m0/m0.off'
-plyPath='/Users/jack/Desktop/privateStuff/UUstuff/2019-2020/period1/MR/m0.off.ply'
-mesh = trimesh.load_mesh(plyPath)
-# # print(len(mesh.edges))
-# mesh.show()
+
+
+#scan the whole database
+def scanDB():
+
+    import os
+    path = "/Users/darkqian/PycharmProjects/MR/LabeledDB"
+    files = os.listdir(path)
+    i = 0
+    for lables in files:
+        try:
+            lable = os.listdir(path + '/' + lables)
+        except:
+            continue
+        for file in lable:
+            if not os.path.isdir(file) and not os.path.splitext(file)[-1] == '.txt':
+                f = path + '/' + lables + "/" + file
+                print(file + ' ' + lables)
+                Meshfilter(f)
+                #viewMesh(f)
+                i += 1
+
+    print(i)
+
+def Meshfilter(filepath):
+
+    mesh = trimesh.load_mesh(filepath)
+
+    print("Number of vertices: ", mesh.vertices.shape[0])
+    print("Number of faces: ", mesh.faces.shape[0])
+    print("Bounding box volume: ", mesh.bounding_box_oriented.volume)
 
 
 
-# edges without duplication
-edges = mesh.edges_unique
-# the actual length of each unique edge
-length = mesh.edges_unique_length
-# create the graph with edge attributes for length
-g = nx.Graph()
-for edge, L in zip(edges, length):
-    g.add_edge(*edge, length=L)
 
 
-# run the shortest path query using length for edge weight
-path=[]
-for each_edge in mesh.edges:
-    path.append(nx.shortest_path(g,source=each_edge[0],target=each_edge[1],weight='length'))
+def viewMesh(filepath):
 
-# VISUALIZE RESULT
-# make the sphere white
-mesh.visual.face_colors = [255,255,255,255]
-# # Path3D with the path between the points
+    #offPath= '/Users/darkqian/PycharmProjects/MR/LabeledDB/Fourleg/397.off'
+    #plyPath= '/Users/darkqian/PycharmProjects/MR/benchmark/db/0/m1/m1.off'
+    mesh = trimesh.load_mesh(filepath)
+    # print(len(mesh.edges))
+    '''
+    mesh.show()
+    
+    # edges without duplication
+    edges = mesh.edges_unique
+    # the actual length of each unique edge
+    length = mesh.edges_unique_length
+    # create the graph with edge attributes for length
+    g = nx.Graph()
+    for edge, L in zip(edges, length):
+        g.add_edge(*edge, length=L)
+    
+    
+    # run the shortest path query using length for edge weight
+    path=[]
+    for each_edge in mesh.edges:
+        path.append(nx.shortest_path(g, source=each_edge[0], target=each_edge[1], weight='length'))
+    
+    # VISUALIZE RESULT
+    # make the sphere white
+    #mesh.visual.face_colors = [255,255,255,255]
+    # # Path3D with the path between the points
+    
+    path_visual = []
+    
+    for eachPath in path:
+        path_visual.append(trimesh.load_path(mesh.vertices[eachPath]))
+    
+    
+    # create a scene with the mesh, path, and points
+    scene = trimesh.Scene([path_visual, mesh])
+    
+    scene.show()
+    '''
 
-path_visual = []
-
-for eachPath in path:
-    path_visual.append(trimesh.load_path(mesh.vertices[eachPath]))
-
-
-# create a scene with the mesh, path, and points
-scene = trimesh.Scene([path_visual, mesh ])
-
-scene.show()
+scanDB()
